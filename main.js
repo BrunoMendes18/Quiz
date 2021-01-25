d1636a8a364075b0684853537e23f4741ca6fab6
xavier-monteiro <xaviermonteiro05@gmail.com>
2021-01-25 12:20:03 +0000

    Comentei o codigo
    Comentei uma parte do codigo

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function chamarAPI()
     .then((response)=>{ return response.text()})
     .then((r) => {respostaAPI =JSON.parse(r); console.log(respostaAPI);novaPergunta()})
 
-    
+    //Fez o pedido e no fim invocou a funçao novaPergunta;
 }
 
 
@@ -67,6 +67,7 @@ function validarResposta(resposta)
 
     if(resposta==respostaAPI.results[contar].correct_answer)
     {
+        //Aqui que vai-se adicionar os pontos
         console.log("acertou");
         contar++;
         novaPergunta();
@@ -89,21 +90,32 @@ function novaPergunta()
         return console.log("jogo acabou, de reset á pagina");
     }
 
+//Voltar a por o display do 3 e 4 botao pk eles desaparecem se a pergunta é de verdadeiro e falso
     btnResposta3.style.display ="inline";
     btnResposta4.style.display ="inline";
+
+    //Atribuir a pergunta ao h1 especifico
     h1Pergunta.innerHTML = respostaAPI.results[contar].question;
+    //Atribuir a categoria ao h1 especifico
     h1Categoria.innerHTML = respostaAPI.results[contar].category;
 
+
+    //Se a pergunta for de verdadeiro e falso
     if(respostaAPI.results[contar].incorrect_answers[0]=="False" || respostaAPI.results[contar].incorrect_answers[0]=="True")
     {
         console.log("pergunta com resposta Verdadeiro ou Falso");
+        //Desaparece o 3 e 4 butao
         btnResposta3.style.display ="none";
         btnResposta4.style.display ="none";
+
+        //Adiciona ao array com Todas as respostas para aquela pergunta(as incorretas+corretas)
         arrayTodasRespostas.push(respostaAPI.results[contar].incorrect_answers);
         arrayTodasRespostas.push(respostaAPI.results[contar].correct_answer);
     }
     else 
     {
+
+        //Faz o mesmo do de cima mas como tem mais respostas fiz um for
         for(var i =0;i<respostaAPI.results[contar].incorrect_answers.length;i++)
     {
         arrayTodasRespostas.push(respostaAPI.results[contar].incorrect_answers[i])
@@ -117,14 +129,20 @@ function novaPergunta()
 
     
     
-    //Isto faz o shuffle das perguntas
+    //Isto faz o shuffle das respostas
     for(var i=0;i<arrayBtnTeste.length;i++)
     {
+        //Random numero entre 0 e array.length(no inicio vai de 0-4 mas vai diminuir pk eu removo do array)
         let numero = Math.floor(Math.random() * arrayTodasRespostas.length);
+
+        //O butao i (neste momento o 1(na posicao 0 fica o primeiro butao eu declarei em cima de todo)) fica com a pergunta duma posicao random do array de respostas
         arrayBtnTeste[i].innerHTML = arrayTodasRespostas[numero];
+
+        //Remove a resposta do array
         arrayTodasRespostas.splice(numero,1);
     }
 
+    //O jogo começou
     GamaHasStarted = true;
    
 }
